3df21460e4bfa0f98da48456d2d45e05475c8256
Henry Li <henry1943@163.com>
2025-04-07 09:40:14 +0800

    feat: get chat model name from `.env`

--- src/models/models.py
+++ src/models/models.py
@@ -1,5 +1,8 @@
+import os
+
 from langchain_openai import ChatOpenAI
 
 
-def get_chat_model(model_name="gpt-4o-2024-11-20", **kwargs):
+def get_chat_model(**kwargs):
+    model_name = os.getenv("CHAT_MODEL", "gpt-4o")
     return ChatOpenAI(model=model_name, temperature=0, max_tokens=4096, **kwargs)
